0c9c33af83a5e11493c8aae0b7aaf195ca9a7340
iaranitamiguel <iaranita.miguel@gmail.com>
2025-03-15 00:08:17 -0300

    integração com o MongoDB

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,15 +1,15 @@
 const mongoose = require ('mongoose')
+require('dotenv').config()
 
 async function conectaBancoDeDados(){
     try {
         console.log('Conexão com o banco de dados iniciou')
 
-        await mongoose.connect('mongodb+srv://iaranitamiguel:TrwdO8YE5KuQP6u7@cluster0.zxim5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        await mongoose.connect(process.env.MONGO_URL)
       
         console.log('Conexão feita  com o banco de dados com sucesso!')
         } catch(erro) {
             console.log(erro)
         }
-
-        module.exports = conectaBancoDeDados
 }
+module.exports = conectaBancoDeDados
--- mulheres.js
+++ mulheres.js
@@ -1,85 +1,53 @@
 const express = require('express')
 const router = express.Router()
-const { v4: uuidv4 } = require('uuid')
+const cors = require('cors')
 
 const conectaBancoDeDados = require('./bancoDeDados.js')
 conectaBancoDeDados()
 
-
+const Mulher = require('./mulherModel.js')
 const app = express()
 app.use(express.json())
+app.use(cors())
 const porta = 3333
 
-const mulheres =  [
+//GET
 
-    {
-      
-      id:'1',
+async function mostraMulheres(request,response) {
 
-      nome: 'Simara Conceição',
-   
-      imagem: 'https://bit.ly/3LJIyOF',
-   
-      minibio: 'Desenvolvedora e instrutora',
-   
-    },
-   
-    {
-      
-      id: '2',
+  try {
+    const mulheresVindasDoBancoDeDados = await Mulher.find()
 
-      nome: 'Iana Chan',
-   
-      imagem: 'https://bit.ly/3JCXBqP',
-   
-      minibio: 'CEO & Founder da PrograMaria',
-   
-    },
-   
-    {
-   
-      id: '3',
-
-      nome: 'Luana Pimentel',
-   
-      imagem: 'https://bit.ly/3FKpFaz',
-   
-      minibio: 'Senior Staff Software Engineer',
-   
-    }
-   
-   ]
-
-function mostraMulheres(request,response) {
-    response.json(mulheres)
-
+    response.json(mulheresVindasDoBancoDeDados)
+    
+  } catch (erro) {
+    console.log(erro)
+  }
 }
 
 // POST
-function criaMulher(request,response) {
-  const novaMulher =  {
-    id: uuidv4(),
+async function criaMulher(request,response) {
+  const novaMulher = new Mulher({
     nome:request.body.nome,
     imagem: request.body.imagem,
-    minibio: request.body.minibio
-  }
-  
-  mulheres.push(novaMulher)
+    minibio: request.body.minibio,
+    citacao: request.body.citacao
+  })
 
-  response.json(mulheres)
+  try {
+    const mulherCriada = await novaMulher.save()
+    response.status(201).json(mulherCriada)
+  } catch (erro){
+    console.log(erro)
+
+  }
 }
 
 //PATCH
-function corrigeMulher(request,reponse) {
-  function encontraMulher(mulher){
-    if (mulher.id === request.params.id) {
-      return mulher
-    }
-  }
-
-  const mulherEncontrada = mulheres.find(encontraMulher)
-
-  if (request.body.nome) {
+async function corrigeMulher(request,reponse) {
+  try { 
+    const mulherEncontrada =  await Mulher.findById(request.params.id) 
+    if (request.body.nome) {
     mulherEncontrada.nome = request.body.nome
   }
 
@@ -90,18 +58,29 @@ function corrigeMulher(request,reponse) {
   if (request.body.imagem) {
     mulherEncontrada.imagem = request.body.imagem
   }
+
+  if(request.body.citacao) {
+    mulherEncontrada.citacao = request.body.citacao
+  }
+
+  const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+
+  response.json(mulherAtualizadaNoBancoDeDados)
+  
+  } catch (erro) {
+    console.log(erro)
+  }
 }
 
 // DELETE
-function deletaMulher(request,response) {
-  function todasMenosEla(mulher){
-    if (mulher.id !== request.params.id) {
-      return mulher
-    }
-  }
+async function deletaMulher(request,response) {
+  try {
+    await Mulher.findByIdAndDelete(request.params.id)
+    response.json({mensagem: "Mulher deletada com sucesso!"})
 
-  const mulheresQueFicam = mulheres.filter(todasMenosEla)
-  response.json(mulheresQueFicam)
+  } catch (erro) {
+    console.log(erro)
+  }
 }
 // PORTA
 function mostrarPorta (){
